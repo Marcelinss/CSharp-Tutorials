086d2a590f9812ca80b9d50e8bcb642ee7e8bb7f
Marcelin Irudayaraj <marcelin.ss@outlook.com>
2020-04-27 18:01:17 +0530

    test

---
+++ CSharp-Tutorials/RefLocalRefReturn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Tutorials
+{
+    class RefLocalRefReturn
+    {
+        public static void Main(string[] args)
+        {
+            int no1 = 1;
+            ref int no2 = ref no1;
+            no2 = 2;
+            Console.WriteLine($"local variable {nameof(no1)} after the change: {no1}");
+
+            Console.WriteLine("Press any key to Exit.");
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
---
+++ CSharp-Tutorials/Yield.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace CSharp_Tutorials
+{
+    class Yield
+    {
+        public static void Main(string[] args)
+        {
+
+            Stopwatch NormalMethodExecutionTime = new Stopwatch();
+            NormalMethodExecutionTime.Start();
+
+            foreach (int i in GetNormalEvenNumbers())
+            {
+                Console.WriteLine(i);
+            }
+            NormalMethodExecutionTime.Stop();
+            Console.WriteLine("Time taken for GetNormalEvenNumbers is " + NormalMethodExecutionTime.Elapsed);
+
+            Stopwatch YieldMethodExecutionTime = new Stopwatch();
+            YieldMethodExecutionTime.Start();
+            foreach (int i in GetYieldEvenNumbers())
+            {
+                Console.WriteLine(i);
+            }
+            YieldMethodExecutionTime.Stop();
+            Console.WriteLine("Time taken for GetYieldEvenNumbers is " + YieldMethodExecutionTime.Elapsed);
+            Console.ReadLine();
+        }
+
+        static IEnumerable<int> GetYieldEvenNumbers()
+        {
+            for(int i=0;i<10;i++)
+            {                
+                if(i%2==0)
+                {
+                    yield return i;
+                }
+            }
+        }
+
+        static IEnumerable<int> GetNormalEvenNumbers()
+        {
+            List<int> lstEven = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    lstEven.Add(i);
+                }
+            }
+
+            return lstEven;
+        }
+    }
+}
